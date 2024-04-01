8f40e5d87b41b1dee7b41feee7720d373eeb6639
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-03-31 22:42:24 -0700

    Fixed infinite loop issue

--- scripts/newton.js
+++ scripts/newton.js
@@ -1,37 +1,40 @@
 const TOLERANCE = 0.001
 
 function sqrt(x) {
+    console.log(`finding sqrt of: ${x}`)
     return sqrt_iter(1, x)
 }
 
 //newton's method for finding square roots
 //recursively iterative
 function sqrt_iter(guess, x) {
-    console.log(guess)
+    console.log(`guess: ${guess}`)
     return is_good_enough(guess, x) ? guess : sqrt_iter(improve(guess, x), x)
 }
 
 function is_good_enough(guess, x) {
+    console.log(`is good enough? ${abs(square(guess) - x) < TOLERANCE}`)
     return abs(square(guess) - x) < TOLERANCE
 }
 
 function abs(x) {
+    console.log(`absolute value is: ${x > 0 ? x : x * -1}`)
     return x > 0 ? x : x * -1
 }
 
 function square(x) {
+    console.log(`square is: ${x * x}`)
     return x * x
 }
 
 function improve(guess, x) {
-    return average(guess, guess / x)
+    console.log(`improvement is: ${average(guess, x / guess)}`)
+    return average(guess, x / guess)
 }
 
 function average(x, y) {
+    console.log(`average is: ${(x + y) / 2}`)
     return (x + y) / 2
 }
-
-//current this leads to an infinite loop
-//TODO: figure out why
 
 console.log(sqrt(9))
